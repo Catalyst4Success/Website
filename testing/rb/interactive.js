cba74700a0f821e6f7fe20bc90648a4547a8e40f
cata3017 <cata3017@p3nlhftpg191.shr.prod.phx3.secureserver.net>
2014-03-29 12:00:33 -0700

    Updated to split each chapter's website into it's own repo.

--- testing/rb/interactive.js
+++
@@ -1,8 +0,0 @@
-
-
-var canvas1= document.getElementById('catalystcanvas');
-var canvas = canvas1.getContext('2d');
-
-
-canvas.font="200px, Arial";
-canvas.fillText ("WELCOME to Catalyst4Success Interact, 10, 50");
